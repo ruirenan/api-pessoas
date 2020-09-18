fd2985f8920d10118f1f49c83023adf066093b29
Rui Renan Peric <ruirenan@gmail.com>
2020-09-18 20:25:32 -0300

    Atualização UML

--- apirest/src/main/java/com/pessoas/apirest/resources/PessoaResource.java
+++ apirest/src/main/java/com/pessoas/apirest/resources/PessoaResource.java
@@ -3,7 +3,6 @@ package com.pessoas.apirest.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
