1d2b4ca0e39a2f08e6a2550f011d485e434b0902
Rui Renan Peric <ruirenan@gmail.com>
2020-09-18 18:20:19 -0300

    API de Salvar Pessoa

--- apirest/src/main/java/com/pessoas/apirest/resources/PessoaResource.java
+++ apirest/src/main/java/com/pessoas/apirest/resources/PessoaResource.java
@@ -5,10 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pessoas.apirest.models.Pessoa;
+import com.pessoas.apirest.models.PessoaFisica;
 import com.pessoas.apirest.repository.PessoaRepository;
 
 @RestController
@@ -23,8 +26,15 @@ public class PessoaResource {
 	public List<Pessoa> listaPessoas(){
 		return pessoaRepository.getAll();
 	}
+	
 	@GetMapping("/pessoas/{id}")
 	public Pessoa listaUmaPessoa(@PathVariable("id") Integer id){
 		return pessoaRepository.getById(id);
 	}
+	
+	@PostMapping("/pessoasFisicas")
+	public Pessoa salvaPessoaFisica(@RequestBody PessoaFisica pessoa) {
+		return pessoaRepository.save(pessoa);
+	}
 }
+
